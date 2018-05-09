80e52d4db987b64a72fc3b81419cbfb8c48cffd8
NoReasonKen <ken5112840801@gmail.com>
2018-05-09 21:50:07 +0800

    finished train and model, but lr of optim may need decay

--- cvae.py
+++ cvae.py
@@ -41,30 +41,42 @@ class CVAE(nn.Module):
         self.conv2 = nn.Sequential(
                 nn.Conv2d(2, 11, kernel_size=3, padding=1),
                 nn.ReLU(),
-                nn.UpsamplingNearest2d(scale_factor=2),
+                nn.Upsample(scale_factor=2),
                 nn.Conv2d(11, 3, kernel_size=3, padding=1),
                 nn.ReLU(),
                 nn.Conv2d(3, 1, kernel_size=3, padding=1),
                 nn.Sigmoid())
 
     def encoder(self, x, c):
-        inputs = torch.cat([x, c], 1)
-        inputs = self.fc1(self.conv1(inputs))
+        onehot = V(torch.zeros(1, 10, 28, 28)).cuda()
+        for i in range(len(c)):
+            tmp = V(torch.zeros(1, 10, 1, 1)).cuda()
+            tmp.data[0][int(c.data[i])][0][0] = 1
+            tmp = tmp.expand(1, -1, 28, 28)
+            onehot = torch.cat([onehot, tmp], 0)
+        onehot = onehot[1:]
+        
+        inputs = torch.cat([x, onehot], 1)
+        inputs = self.fc1(self.conv1(inputs).view(-1, 1, 784))
         return self.fc21(inputs), self.fc22(inputs)
 
     def reparameterize(self, u, logv):
         if self.training:
             std = torch.exp(0.5 * logv)
-            eps = torch.randn_like(std)
+            eps = V(torch.randn(len(u), 1, 20)).cuda()
             return eps.mul(std).add_(u)
 
     def decoder(self, z, c):
-        inputs = torch.cat([z, c], 1)
-        inputs = self.conv2(self.fc3(inputs))
+        onehot = V(torch.zeros(len(c), 1, 10)).cuda()
+        for i in range(len(c)):
+            onehot.data[i][0][int(c.data[i])] = 1
+        inputs = torch.cat([z, onehot], 2)
+        inputs = self.conv2(self.fc3(inputs).view(-1, 2, 14, 14))
         return inputs
     
     def forward(self, x, c):
-        return self.decoder(self.reparameterize(self.encoder(x, c)), c)
+        u, logv = self.encoder(x, c)
+        return self.decoder(self.reparameterize(u, logv), c), u, logv
         
 
 #====================================================================
@@ -86,7 +98,7 @@ model = CVAE().cuda()
 optimizer = optim.Adam(model.parameters(), lr=LR)
 
 def criterion(output, data, u, logv):
-    MSE = F.mse_loss(output, data.view(-1, 784), size_average=False)
+    MSE = F.mse_loss(output, data, size_average=False)
     KLD = -0.5 * torch.sum(1 + logv - u.pow(2) - logv.exp())
     return MSE + KLD
 
@@ -98,16 +110,8 @@ def train(epoch):
     for idx, (data, label) in enumerate(train_loader):
         optimizer.zero_grad()
         data, label = V(data.cuda()), V(label.float().cuda())
-        zero = V(torch.zeros(1, 10, 28, 28))
-        onehot = V(torch.zeros_like(zero.data))
-        for i in range(len(label)):
-            tmp = V(torch.zeros(1, 10, 1, 1))
-            tmp.data[0][int(label.data[i])][0][0] = 1
-            tmp = tmp.expand(1, -1, 28, 28)
-            onehot = torch.cat([onehot, tmp], 0)
-        onehot = onehot[1:].cuda()
 
-        output, u, logv = model(data, onehot)
+        output, u, logv = model(data, label)
         delta = criterion(output, data, u, logv)
         delta.backward()
         optimizer.step()
--- utils.py
+++ utils.py
@@ -6,6 +6,12 @@ import math
 import torch.nn as nn
 import torch.nn.init as init
 
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+TOTAL_BAR_LENGTH=65
+
+last_time = time.time()
+begin_time = last_time
 def progress_bar(current, total, msg=None):
     global last_time, begin_time
     if current == 0:
@@ -48,3 +54,35 @@ def progress_bar(current, total, msg=None):
     else:
         sys.stdout.write('\n')
     sys.stdout.flush()
+
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
